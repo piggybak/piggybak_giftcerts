c61e90d793a9fbe0e6b511fba83432e2da94b143
Steph Skardal <steph@endpoint.com>
2012-11-01 13:19:04 -0400

    Final migration cleanup, named routes for link.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 PiggybakGiftcerts::Engine.routes.draw do
   match "/apply_giftcert" => "giftcert#apply"
-  match "/giftcert" => "giftcert#purchase"
+  match "/giftcert" => "giftcert#purchase", :as => "buy_giftcert"
 end
--- db/migrate/20121026200953_piggybak_giftcert_setup.rb
+++ db/migrate/20121026200953_piggybak_giftcert_setup.rb
@@ -15,12 +15,31 @@ class PiggybakGiftcertSetup < ActiveRecord::Migration
       t.timestamps
     end
 
-    ::PiggybakGiftcerts::BuyableGiftcert.create()
+    gc_vals = [10, 20, 40]
+
+    option = ::PiggybakVariants::Option.new({ :name => "Amount", :position => 1 })
+    option.option_configurations << ::PiggybakVariants::OptionConfiguration.new({ :klass => "PiggybakGiftcerts::BuyableGiftcert" })
+    gc_vals.each { |val| option.option_values << ::PiggybakVariants::OptionValue.new({ :name => "$#{val}", :position => 1 }) }
+    option.save
+
+    bg = ::PiggybakGiftcerts::BuyableGiftcert.new
+    gc_vals.each do |val|
+      s = ::Piggybak::Sellable.new({ :sku => "giftcert-#{val}", :description => "$#{val} Gift Certificate", :price => val, :active => true, :unlimited_inventory => true }) 
+      v = ::PiggybakVariants::Variant.new
+      v.piggybak_sellable = s
+      v.option_values << ::PiggybakVariants::OptionValue.find_by_name("$#{val}")
+      bg.variants << v
+    end
+
+    bg.save
   end
 
   def down
     drop_table :giftcerts
     drop_table :buyable_giftcerts
     drop_table :giftcert_applications
+
+    option = ::PiggybakVariants::Option.find_by_name("Amount")
+    ::PiggybakVariants::Option.destroy(option)
   end
 end
