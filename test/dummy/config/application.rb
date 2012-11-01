446e3106e8b30f690b010b5968d05e0cce483a0a
Steph Skardal <steph@endpoint.com>
2012-11-01 13:23:18 -0400

    More release prep.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require
-require "piggybak_coupons"
+require "piggybak_giftcerts"
 
 module Dummy
   class Application < Rails::Application
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 
-  mount PiggybakCoupons::Engine => "/piggybak_coupons"
+  mount PiggybakGiftcerts::Engine => "/piggybak_giftcerts"
 end
