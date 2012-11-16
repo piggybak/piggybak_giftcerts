f8b05ce039d50bd38010093ee9232a73d8e894b8
Steph Skardal <steph@endpoint.com>
2012-11-16 15:32:52 -0500

    Percent fix to accomodate coupons.

--- lib/piggybak_giftcerts/percent_decorator.rb
+++ lib/piggybak_giftcerts/percent_decorator.rb
@@ -10,10 +10,16 @@ module PiggybakGiftcerts
             next if (i[:sellable].item.is_a?(::PiggybakVariants::Variant) && i[:sellable].item.item_type == "PiggybakGiftcerts::BuyableGiftcert")
             taxable_total += i[:quantity]*i[:sellable].price
           end
+          taxable_total += object.extra_data[:reduce_tax_subtotal].to_f
         else
           object.line_items.sellables.each do |li|
             next if (li.sellable.item.is_a?(::PiggybakVariants::Variant) && li.sellable.item.item_type == "PiggybakGiftcerts::BuyableGiftcert")
             taxable_total += li.quantity*li.sellable.price
+          end
+          Piggybak.config.line_item_types.each do |k, v|
+            if v.has_key?(:reduce_tax_subtotal) && v[:reduce_tax_subtotal]
+              taxable_total += object.send("#{k}_charge")
+            end
           end
         end
         (method.metadata.detect { |m| m.key == "rate" }.value.to_f * taxable_total).to_c
